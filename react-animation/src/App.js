0cd6c258f7d96b3f5c60af6dd930fef6305fcf60
Teodor Dimitrov <teodor@kipo.bg>
2022-02-03 15:54:33 +0200

    transition component

--- react-animation/src/App.js
+++ react-animation/src/App.js
@@ -4,11 +4,12 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
-
+import Transition from 'react-transition-group/Transition';
 class App extends Component {
 
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -23,11 +24,54 @@ class App extends Component {
     });
   }
 
+  showBlock = () => {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        showBlock: !prevState.showBlock
+      }
+
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
+        <button className="Button" onClick={()=>  this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
+        <br />
+
+     {/* {this.state.showBlock ? (
+          <div style={{
+            backgroundColor: 'red',
+            width: 100,
+            height: 100,
+            margin: 'auto'
+          }}></div>
+        ): null } */}
+        {/* <Transition 
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          >
+
+         {state => (
+            <div style={{
+              backgroundColor: 'red',
+              width: 100,
+              height: 100,
+              margin: 'auto',
+              transition: 'all 1s ease-out',
+              opacity: state === 'exiting' ? 0 : 1
+            }}></div>
+        )
+        }
+        </Transition> */}
+
+     
+
+        <Modal  show={this.state.modalIsOpen} closed={this.closeModal} />
         <Backdrop show={this.state.modalIsOpen}/>
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
--- react-animation/src/components/Modal/Modal.js
+++ react-animation/src/components/Modal/Modal.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+}
+
 const modal = (props) => {
-    const cssClasses = ['Modal', props.show ? 'ModalOpen': 'ModalClosed'];
-    return <div className={cssClasses.join(' ')}>
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
+    return (
+        <Transition 
+            in={props.show} 
+            timeout={animationTiming} 
+            mountOnEnter
+            unmountOnExit>
+            {state => {
+                const cssClasses = [
+                    'Modal',
+                    state === 'entering' ? 'ModalOpen' :
+                    state === 'exiting' ? 'ModalClosed': null
+                ];
+                return (
+                    <div className={cssClasses.join(' ')}>
+                        <h1>A Modal</h1>
+                        <button className="Button" onClick={props.closed}>Dismiss</button>
+                    </div>
+                )
+            }}
+      </Transition>
+    
+   )
 };
 
 export default modal;
