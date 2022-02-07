dceb85e79c8bbaf4987959ffb222fbbfab13b6ab
Teodor Dimitrov <teodor@kipo.bg>
2022-02-07 09:55:43 +0200

    testing async code

--- react-testing-app/src/components/Greeting.js
+++ react-testing-app/src/components/Greeting.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Output from './Output';
 
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  }
+
+
   return <div>
     <h2>Hello World!</h2>
-    <p>It's good to see you!</p>
+    {!changedText && <Output>It's good to see you!</Output>}
+    {changedText && <Output>Changed!</Output>}
+    <button onClick={changeTextHandler}>Change Text!</button>
   </div>;
 };
 export default Greeting;
